a0732626c36a27d06aedd573c3b17010ff20e19f
Alessandro Musolino <alessandromusolino196@gmail.com>
2020-01-29 20:05:09 +0100

    no message

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
 
 app.use(express.static('public'));
 
-router.get('*',function(req,res){
+app.get('*',function(req,res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
